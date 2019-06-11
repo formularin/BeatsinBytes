d3589b3856e2256b966f554e75799892adedb091
lol cubes <kaivernooy@gmail.com>
2019-06-10 23:22:27 -0400

    Added test files and changed directory orangization

---
+++ tests/test_analysisTools.py
@@ -0,0 +1,12 @@
+import unittest
+import pyBiB.analysisTools
+
+
+class TestAnalysisTools(unittest.TestCase):
+
+    def test_find_key_signature(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_musicalAnalysis.py
@@ -0,0 +1,15 @@
+import unittest
+import pyBiB.musicalAnalysis
+
+
+class TestKern(unittest.TestCase):
+
+    def test_composer(self):
+        pass
+
+    def test_title(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
